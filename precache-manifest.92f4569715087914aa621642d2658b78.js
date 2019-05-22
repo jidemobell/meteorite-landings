651cde72fb2e0d4d1e82506e23797e9876ddd41f
jidemobell <jidd32@gmail.com>
2019-05-22 15:08:10 +0100

    Updates

---
+++ precache-manifest.92f4569715087914aa621642d2658b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e96158dbb752aeb3e7561e5b4a11926",
+    "url": "/meteor-landings/index.html"
+  },
+  {
+    "revision": "f45ea8e46485fbe5a968",
+    "url": "/meteor-landings/static/css/2.77155d0b.chunk.css"
+  },
+  {
+    "revision": "cb02f51f98b22561cad7",
+    "url": "/meteor-landings/static/css/main.4f06a95c.chunk.css"
+  },
+  {
+    "revision": "f45ea8e46485fbe5a968",
+    "url": "/meteor-landings/static/js/2.ea313fbf.chunk.js"
+  },
+  {
+    "revision": "cb02f51f98b22561cad7",
+    "url": "/meteor-landings/static/js/main.165eae66.chunk.js"
+  },
+  {
+    "revision": "57bcefa39ae23ef23396",
+    "url": "/meteor-landings/static/js/runtime~main.fe0942f2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cde36e04bc9e650991eb31bd1a5ac72b.js"
+  "/meteor-landings/precache-manifest.92f4569715087914aa621642d2658b78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meteor-landings/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
